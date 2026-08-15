25831803822a09e1e0c5c3fba01e1adf26c0e985
ThisIsSomeone <a.s.farzad3@gmail.com>
2026-08-15 19:08:31 +0200

    Updated taglines

--- src/config.ts
+++ src/config.ts
@@ -88,7 +88,7 @@ export const profileConfig: ProfileConfig = {
 export const studioProfileConfig: ProfileConfig = {
 	avatar: "assets/images/ACSLogo.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Alette Creative Studio",
-	bio: "A cozy indie creative corner.",
+	bio: "My small creative corner",
 	links: [
         {
             name: "itch.io",
