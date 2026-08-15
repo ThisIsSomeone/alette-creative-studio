bc8936810b3a0150bee8d78f2c75e2b6857b1519
ThisIsSomeone <a.s.farzad3@gmail.com>
2026-08-15 21:31:27 +0200

    Added favicon

--- src/config.ts
+++ src/config.ts
@@ -30,6 +30,10 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
+        {
+            src: "/favicon/ACSLogo.png",
+            sizes: "32x32"
+        }
 		// Leave this array empty to use the default favicon
 		// {
 		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
